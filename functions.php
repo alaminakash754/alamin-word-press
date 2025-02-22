2851f98cef4ba96ad46551662c5794b314e07d96
alamin <alaminakash9754@gmail.com>
2025-02-22 13:04:35 +0600

    added my first wordpress theme

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php 
+/*
+* my theme function
+*/
+// theme title
+add_theme_support('title-tag');
+
+// theme css and jquery file calling
+function css_js_file_calling(){
+    wp_enqueue_style( 'aw-style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.3.0', 'all' );
+    wp_enqueue_style('bootstrap');
+
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style('custom');
+
+    // jquery
+    // wp_enqueue_script( $handle:string, $src:string, $deps:array, $ver:string|boolean|null, $in_footer:boolean )
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.3.0', 'true');
+    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
+
+}
+add_action( 'wp_enqueue_scripts', 'css_js_file_calling');
+
+// theme function
+function aw_customizer_register($wp_customize){
+    $wp_customize->add_section('aw_header_area', array(
+        'title' =>__('Header Area', 'alaminakash'),
+        'description' => 'you can update logo from here.'
+    ));
+
+    $wp_customize->add_setting('aw_logo', array(
+        'default' => get_bloginfo( 'template_directory' ). '/img/logo.png',
+    ));
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'aw_logo', array(
+        'label' => 'Logo Upload',
+        'setting' => 'aw_logo',
+        'description' => ' If you interested to change or update your logo you can do it',
+        'section' => 'aw_header_area'
+    )));
+}
+add_action( 'customize_register', 'aw_customizer_register');
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+*
+*/
+?>
+
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="header_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+            
+                     <a href=""><img src="<?php echo get_theme_mod( 'aw_logo')?>" alt=""></a>
+                </div>
+                <div class="col-md-9">
+                    <ul id="nav">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About us</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Features</a></li>
+                        <li><a href="">Media</a></li>
+                        <li><a href="">Service</a>
+                    <ul>
+                        <li><a href="">Dropdown Menu</a></li>
+                        <li><a href="">Dropdown Menu</a></li>
+                        <li><a href="">Dropdown Menu</a>
+                    <ul>
+                        <li><a href="">Service1</a></li>
+                        <li><a href="">Service2</a></li>
+                        <li><a href="">Service3</a></li>
+                    </ul>
+                    </li>
+                    </ul>
+                    </li>
+                        <li><a href="">Project</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php wp_footer(); ?>
+</body>
+</html>
