72bc97b00b35fd7cd6bdb50fe58b9b27c38eb9e1
alamin <alaminakash9754@gmail.com>
2025-02-23 23:50:02 +0600

    fixed some error

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <div></div>
                 <?php the_content(  ); ?>
             </div>
         </div>
