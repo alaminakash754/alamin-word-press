837efa49b26e22b73f9abf3a0456f227fb1405a1
alamin <alaminakash9754@gmail.com>
2025-03-02 10:37:19 +0600

    decorate header and footer code in all tamplate

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer id="footer_area">
+    <div id="copyright_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        <?php echo get_theme_mod( 'aw_footer_section'); ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
