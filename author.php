b1e8453fab20241fefd67ee2d6232f6a95b242eb
alamin <alaminakash9754@gmail.com>
2025-03-02 10:06:03 +0600

    added author page

---
+++ author.php
@@ -0,0 +1,57 @@
+<?php
+/*
+*
+*/
+?>
+
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header id="header_area" class="<?php echo get_theme_mod( 'aw_menu_position')?>">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+            
+                     <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod( 'aw_logo')?>" alt=""></a>
+                </div>
+                <div class="col-md-9">
+                    <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
+                </div>
+            </div>
+        </div>
+</header>
+<h1>This is author page ,</h1>
+<section id="body_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<footer id="footer_area">
+    <div id="copyright_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        <?php echo get_theme_mod( 'aw_footer_section'); ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
