8de8830b5d0dd240d44218279626dff75d6649a1
alamin <alaminakash9754@gmail.com>
2025-03-02 10:52:14 +0600

    decorate and theme slicing

--- functions.php
+++ functions.php
@@ -2,110 +2,17 @@
 /*
 * my theme function
 */
-// theme title
-add_theme_support('title-tag');
+// all default theme function is here
+include_once('inc/default.php');
+
 
 // theme css and jquery file calling
-function css_js_file_calling(){
-    wp_enqueue_style( 'aw-style', get_stylesheet_uri());
-    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.3.0', 'all' );
-    wp_enqueue_style('bootstrap');
-
-    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all' );
-    wp_enqueue_style('custom');
-
-    // jquery
-    // wp_enqueue_script( $handle:string, $src:string, $deps:array, $ver:string|boolean|null, $in_footer:boolean )
-    wp_enqueue_script( 'jquery' );
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.3.0', 'true');
-    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true');
-
-}
-add_action( 'wp_enqueue_scripts', 'css_js_file_calling');
-
-// google font enqueue
-function aw_add_google_fonts(){
-    wp_enqueue_style('aw_google_fonts', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald&display=swap" rel="stylesheet', false);
-}
-add_action( 'wp_enqueue_scripts', 'aw_add_google_fonts');
-
+include_once('inc/enqueue.php');
 
 
 // theme function
-function aw_customizer_register($wp_customize){
-    // Header area function
-    $wp_customize->add_section('aw_header_area', array(
-        'title' =>__('Header Area', 'alaminakash'),
-        'description' => 'you can update logo from here.'
-    ));
+include_once('inc/theme_function.php');
 
-    $wp_customize->add_setting('aw_logo', array(
-        'default' => get_bloginfo( 'template_directory' ). '/img/logo.png',
-    ));
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'aw_logo', array(
-        'label' => 'Logo Upload',
-        'setting' => 'aw_logo',
-        'description' => ' If you interested to change or update your logo you can do it',
-        'section' => 'aw_header_area'
-    )));
-
-    // Menu position option
-    $wp_customize->add_section('aw_menu_option', array(
-        'title' => __('Menu Position Option', 'alaminakash'),
-       'description' => 'you can change menu position from here.'
-    ) );
-    $wp_customize->add_setting('aw_menu_position', array(
-        'default'=> 'right_menu'
-    ));
-    $wp_customize-> add_control('aw_menu_position', array(
-        'label'=> 'Menu Position',
-        'description' => 'Select Your Menu Position',
-        'setting' => 'aw_menu_position',
-        'section' => 'aw_menu_option',
-        'type' => 'radio',
-        'choices' => array(
-            'left_menu' => 'Left Menu',
-            'right_menu' => 'Right Menu',
-            'center_menu' => 'Center Menu',
-        ),
-    ));
-
-    // Footer option
-    $wp_customize->add_section('aw_footer_option', array(
-        'title' => __('Footer Option', 'alaminakash'),
-       'description' => 'you can change or update footer text from here.'
-    ) );
-    $wp_customize->add_setting('aw_footer_section', array(
-        'default'=> '&copy; Copyright 2025 | Alamin BD' ,
-    ));
-    $wp_customize-> add_control('aw_footer_section', array(
-        'label'=> 'Copyright Text',
-        'description' => 'You can change or update footer text from here',
-        'setting' => 'aw_footer_section',
-        'section' => 'aw_footer_option',
-    ));
-}
-add_action( 'customize_register', 'aw_customizer_register');
 
 // menu register
-register_nav_menu( 'main_menu', __('Main Menu', 'alaminakash'));
-
-//walker menu properties
-// function aw_nav_description ($item_output, $item, $args){
-//     if( !empty ($item -> description)){
-//         $item_output = str_replace( $args -> link_after . '</a>' , '<span class="walker_nav">' .
-//         $item -> description . '</span>' . $args->link_after . '</a>' , $item_output);
-//     }
-//     return $item_output;
-// }
-
-// add_filter( 'walker_nav_menu_start_el', 'aw_nav_description', 10, 3 );
-
-function aw_nav_description($item_output, $item, $args) {
-    if (!empty($item->description)) {
-        $item_output = str_replace('</a>', '<span class="walker_nav">' . esc_html($item->description) . '</span></a>', $item_output);
-    }
-    return $item_output;
-}
-
-add_filter('walker_nav_menu_start_el', 'aw_nav_description', 10, 3);
+include_once('inc/menu_register.php');
---
+++ inc/theme_function.php
@@ -0,0 +1,56 @@
+<?php
+
+function aw_customizer_register($wp_customize){
+    // Header area function
+    $wp_customize->add_section('aw_header_area', array(
+        'title' =>__('Header Area', 'alaminakash'),
+        'description' => 'you can update logo from here.'
+    ));
+
+    $wp_customize->add_setting('aw_logo', array(
+        'default' => get_bloginfo( 'template_directory' ). '/img/logo.png',
+    ));
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'aw_logo', array(
+        'label' => 'Logo Upload',
+        'setting' => 'aw_logo',
+        'description' => ' If you interested to change or update your logo you can do it',
+        'section' => 'aw_header_area'
+    )));
+
+    // Menu position option
+    $wp_customize->add_section('aw_menu_option', array(
+        'title' => __('Menu Position Option', 'alaminakash'),
+       'description' => 'you can change menu position from here.'
+    ) );
+    $wp_customize->add_setting('aw_menu_position', array(
+        'default'=> 'right_menu'
+    ));
+    $wp_customize-> add_control('aw_menu_position', array(
+        'label'=> 'Menu Position',
+        'description' => 'Select Your Menu Position',
+        'setting' => 'aw_menu_position',
+        'section' => 'aw_menu_option',
+        'type' => 'radio',
+        'choices' => array(
+            'left_menu' => 'Left Menu',
+            'right_menu' => 'Right Menu',
+            'center_menu' => 'Center Menu',
+        ),
+    ));
+
+    // Footer option
+    $wp_customize->add_section('aw_footer_option', array(
+        'title' => __('Footer Option', 'alaminakash'),
+       'description' => 'you can change or update footer text from here.'
+    ) );
+    $wp_customize->add_setting('aw_footer_section', array(
+        'default'=> '&copy; Copyright 2025 | Alamin BD' ,
+    ));
+    $wp_customize-> add_control('aw_footer_section', array(
+        'label'=> 'Copyright Text',
+        'description' => 'You can change or update footer text from here',
+        'setting' => 'aw_footer_section',
+        'section' => 'aw_footer_option',
+    ));
+}
+add_action( 'customize_register', 'aw_customizer_register');
