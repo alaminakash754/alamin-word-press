b8bb3c7da4f79799a19c7f49a853f0d47f73b3d3
alamin <alaminakash9754@gmail.com>
2025-03-04 13:11:54 +0600

    added read more button for showing full post

--- inc/default.php
+++ inc/default.php
@@ -4,3 +4,17 @@ add_theme_support('title-tag');
 // Thumbnail image area
 add_theme_support( 'post-thumbnails', array('page', 'post'));
 add_image_size( 'post-thumbnails', 970 , 350, true );
+
+// excerpt to 40 word
+
+function aw_excerpt_more($more){
+    global $post;
+    return '<br> <br><a class="readmore" href="'.get_permalink($post -> ID) . '">' . ' Read More' . '</a>';
+}
+add_filter( 'excerpt_more','aw_excerpt_more' );
+
+function aw_excerpt_length($length){
+   return 20;
+}
+add_filter( 'excerpt_length','aw_excerpt_length' );
+
