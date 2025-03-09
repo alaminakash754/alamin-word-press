1870107517237b1b69cad47a81f8e3649889f9ed
alamin <alaminakash9754@gmail.com>
2025-03-09 11:38:35 +0600

    added search page and decorate it

--- search.php
+++ search.php
@@ -1,14 +1,16 @@
 <?php
+
+/*
+* displaying search result
+*/
 get_header();
 ?>
 <section id="body_area">
     <div class="container">
         <div class="row">
             <div class="col-md-9">
-                <div id="archive_title">
-                    <?php the_archive_title( '<h1 class="title">','</h1>');
-                    the_archive_description( '<div class="description">','</div>');
-                    ?>
+                <div id="search_title">
+                    <h1 class="title"><?php printf(__('Search Results For: %s', 'alaminakash'),get_search_query()); ?></h1>
                 </div>
                 <?php get_template_part('template_part/blog_setup'); ?>
             </div>
