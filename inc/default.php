0b044931b6ea657a3ebaa96876b3f243759f76ad
alamin <alaminakash9754@gmail.com>
2025-03-14 12:07:22 +0600

    change in header area

--- inc/default.php
+++ inc/default.php
@@ -4,7 +4,7 @@ add_theme_support('title-tag');
 // Thumbnail image area
 add_theme_support( 'post-thumbnails', array('page', 'post', 'service','slider',));
 add_image_size( 'service', 400, 350, true );
-add_image_size( 'slider', 1200, 400, true );
+add_image_size( 'slider', 1200, 300, true );
 add_image_size( 'post-thumbnails', 970 , 350, true );
 
 // excerpt to 40 word
