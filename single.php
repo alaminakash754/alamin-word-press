16426532619e24f053bb1e9760f284906bb935be
alamin <alaminakash9754@gmail.com>
2025-03-17 11:22:03 +0600

    comments allow or disallow option added in post

--- single.php
+++ single.php
@@ -12,7 +12,9 @@ get_header();
             <div class="col-md-9">
                 <?php get_template_part('template_part/post_setup'); ?>
                 <div id="comment_area">
+                    <?php if (comments_open( )) : ?>
                     <?php comments_template(); ?>
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="col-md-3">
