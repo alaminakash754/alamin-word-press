a4faec0a4bec64d90db84915304a1522b0ddd74a
alamin <alaminakash9754@gmail.com>
2025-03-21 11:11:58 +0600

    redesgin attachment file

--- attachment.php
+++ attachment.php
@@ -9,16 +9,9 @@ get_header();
 <section id="body_area">
     <div class="container">
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <?php get_template_part('template_part/post_setup'); ?>
-                <div id="comment_area">
-                    <?php if (comments_open( )) : ?>
-                    <?php comments_template(); ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <?php get_sidebar(); ?>
+                
             </div>
         </div>
     </div>
